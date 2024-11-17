8a2763d4c7c5f6141f812d90027c73b37e341035
Your Name <wjddl5796@naver.com>
2024-11-17 21:53:19 +0900

    Feat : fretboard test

--- fe/src/app/component/fretboard.js
+++ fe/src/app/component/fretboard.js
@@ -1,22 +1,21 @@
 // fretboard.js
 'use client';
-import { useEffect, useState } from "react";
-import "../style/css/fretboard.css";
-import { MenuItem, Select } from "@mui/material";
+import { useEffect, useState } from 'react';
+import '../style/css/fretboard.css';
+import { MenuItem, Select } from '@mui/material';
 
 export default function Fretboard({ tune, string, chordToneAr, keySign, fret, highlightedOnly = false }) {
-
-  const sharpNote = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-  const flatNote = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"];
+  const sharpNote = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+  const flatNote = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
 
   const [tuneValues, setTuneValues] = useState(tune.slice(0, string));
-
 
   function setFret(startTune, fretCount = fret) {
     return Array.from({ length: fretCount }, (_, fret) => ((startTune + fret) % 12 == 0 ? 12 : (startTune + fret) % 12));
   }
-
-
+  useEffect(() => {
+    setTuneValues(tune.slice(0, string));
+  }, [string]);
 
   function selectTune(value, index) {
     setTuneValues((prev) => {
@@ -24,36 +23,49 @@ export default function Fretboard({ tune, string, chordToneAr, keySign, fret, hi
       newTune[index] = value;
       return newTune;
     });
-  };
-
+  }
 
   return (
     <div className="fretboard">
       <div className="zeroFret">
-        {tune.slice(0, string).map((_, stringIndex) => {
+        {tuneValues.slice(0, string).map((_, stringIndex) => {
           return (
-            <Select key={stringIndex} className="tune-select" value={tuneValues[stringIndex]} onChange={(event) => selectTune(event.target.value, stringIndex)}
-            >
+            <Select key={stringIndex} className="tune-select" value={tuneValues[stringIndex]} onChange={(event) => selectTune(event.target.value, stringIndex)}>
               <MenuItem value={4}>C</MenuItem>
-              <MenuItem value={5}>{keySign ? "C#" : "Db"}</MenuItem>
+              <MenuItem value={5}>{keySign ? 'C#' : 'Db'}</MenuItem>
               <MenuItem value={6}>D</MenuItem>
-              <MenuItem value={7}>{keySign ? "D#" : "Eb"}</MenuItem>
+              <MenuItem value={7}>{keySign ? 'D#' : 'Eb'}</MenuItem>
               <MenuItem value={8}>E</MenuItem>
               <MenuItem value={9}>F</MenuItem>
-              <MenuItem value={10}>{keySign ? "F#" : "Gb"}</MenuItem>
+              <MenuItem value={10}>{keySign ? 'F#' : 'Gb'}</MenuItem>
               <MenuItem value={11}>G</MenuItem>
-              <MenuItem value={12}>{keySign ? "G#" : "Ab"}</MenuItem>
+              <MenuItem value={12}>{keySign ? 'G#' : 'Ab'}</MenuItem>
               <MenuItem value={1}>A</MenuItem>
-              <MenuItem value={2}>{keySign ? "A#" : "Bb"}</MenuItem>
+              <MenuItem value={2}>{keySign ? 'A#' : 'Bb'}</MenuItem>
               <MenuItem value={3}>B</MenuItem>
             </Select>
           );
         })}
       </div>
+      <div className="noteFret">
+        {tuneValues.map((startTune, stringIndex) => (
+          <div className="string" key={stringIndex}>
+            {setFret(startTune + 1).map((fretNote, fretIndex) => {
+              const noteLabel = keySign ? sharpNote[fretNote - 1] : flatNote[fretNote - 1];
+              return (
+                <div className="fret" key={fretIndex}>
+                  {noteLabel}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 
-  {/* 
+  {
+    /* 
         {defaultTune.slice(0, string).map((startTune, stringIndex) => (
           <div key={`string-${stringIndex}`} className={"string"}>
             {setFret(startTune).map((fretNote, fretIndex) => {
@@ -69,8 +81,7 @@ export default function Fretboard({ tune, string, chordToneAr, keySign, fret, hi
               );
             })}
           </div>
-        ))} */}
-    
-    
+        ))} */
+  }
 }
 // 선택된 음들만 표기 , 튜닝 변경 기능 추가 , 프렛카운트 조절 추가 , 기본 튜닝으로 복귀 기능 추가
